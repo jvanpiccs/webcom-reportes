935083f7f17d0433c1f1c980069330c9efbdefe0
jvanpiccs <9990398+jvanpiccs@users.noreply.github.com>
2022-02-21 21:41:29 -0300

    first commit

---
+++ src/webparts/webcomReportes/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IWebcomReportesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'WebcomReportesWebPartStrings' {
+  const strings: IWebcomReportesWebPartStrings;
+  export = strings;
+}
